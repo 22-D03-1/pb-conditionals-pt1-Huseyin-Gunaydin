9102858cd2d74d4577684f95953829baeb414b18
= <=>
2022-07-07 12:18:32 +0200

    solution

---
+++ solution.js
@@ -0,0 +1,106 @@
+// Frage1
+// var zahlen1=80, zahlen2=80;
+
+// if((zahlen1 >= 50 && zahlen1<=99)&&(zahlen2 >= 50 && zahlen2<=99)) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// Frage 2
+
+// var zahlen1=48, zahlen2=49, zahlen3=50;
+
+// if((zahlen1 >= 50 && zahlen1<=99)||(zahlen2 >= 50 && zahlen2<=99)||(zahlen3 >= 50 && zahlen3<=99)
+
+// ) {
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+//Frage 3
+
+// var a=1, b=2, c=3;
+
+// if(a > b && a > c){
+//     console.log("a ist am grösten.");
+// }
+// else if (b > a && b > c){
+//     console.log("b ist am grösten.");
+// }
+// else if (c > a && c > b){
+//     console.log("c ist am grösten.");
+// }
+// else{
+//     console.log("Zahlen sind gleich");
+// }
+
+//Frage 4
+
+
+
+//Frage 5
+
+// var a=25, b=50;
+// if((a+b>50) && (a+b<80)){
+//     console.log("65");
+// }else(
+//     console.log("80")
+// )
+
+//Frage 6
+ var a=10, b=2;
+
+if((a+b==8)|| Math.abs(a-b==8)){
+    console.log("Richtig");
+}else(
+    console.log("Falsch")
+)
+
+//Frage 7
+//  var a=10, b=5;
+
+// if(((a==15)||(b==15)) ||(a+b==15)){
+//     console.log("Richtig");
+// }else(
+//     console.log("Falsch")
+// )
+
+//Frage 8
+// var a=4, b=11;
+// if((a%7==0 || b%7==0)||(a%11==0 || b%11==0)){
+//     console.log("richtig");
+// }else{
+//     console.log("falsch")
+// }
+
+//Frage 9
+
+
+
+
+
+
+// //Frage 10
+// var a=39;
+// if(Math.abs(a-19>19)){
+//     console.log(Math.abs((a-19)*2))
+// }else{
+//     console.log("weiter versuchen")
+// }
+
+// Bonus 11
+
+// var firstName = "Hüseyin";
+// var age = 11;
+
+// if (age < 13) {
+//   console.log(`${firstName} ist ein Kind`);
+// } else if (age >= 13 || age < 20) {
+//   console.log(`${firstName} ist ein Teenager`);
+// } else if (age >= 20 || age < 30) {
+//   console.log(`${firstName} ist ein junger Erwachsene`);
+// } else {
+//   console.log(`${firstName}  ist ein Erwachsener`);
+// }
